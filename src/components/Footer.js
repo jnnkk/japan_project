a58c73bed6b2b616f17e508e2cb4ae2d2247a7f5
SJY <jun000405@naver.com>
2024-05-29 14:58:51 +0900

    chore: footer text chage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
     <footer className="bg-light text-center py-4">
       <a className='m-2 p-2' href='https://www.instagram.com/songjy0405/'><img src='img/instagram.svg' alt='...' style={linkIcon}></img></a>
       <a className='m-2 p-2' href='https://github.com/jnnkk'><img src='img/github.svg' alt='...' style={linkIcon}></img></a>
-      <p className='mt-2'>&copy; 2024 Japan Travel Guide. All rights reserved.</p>
+      <p className='mt-2'>&copy; 2024 Japan Travel Guide. SJY Web Programming.</p>
     </footer>
   );
 }
