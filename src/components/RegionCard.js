cbcbb98daab967c39c386277cf78bada5d793c7f
SJY <jun000405@naver.com>
2024-06-12 12:45:35 +0900

    feat: RegionCard.moudle.css blur add

--- src/components/RegionCard.js
+++ src/components/RegionCard.js
@@ -11,10 +11,10 @@ function RegionCard({regionName, background, url}) {
 
     return (
         <div className="col-md-4 mb-5">
-            <div className={`${styles.card} card text-bg-dark`} onClick={() => handleClick(`${url}`)}>
+            <div className={`${styles.card} card text-bg-dark position-relative`} onClick={() => handleClick(`${url}`)}>
                 <img src={`${process.env.PUBLIC_URL}/img/${background}`} className="card-img" alt="..."></img>
                 <div className={`${styles.cardimgoverlay} card-img-overlay`}>
-                    <h3 className="card-title">{regionName}</h3>
+                    <h3 className={`${styles.title} card-title position-absolute top-50 start-50 translate-middle`}>{regionName}</h3>
                 </div>
             </div>
         </div>
